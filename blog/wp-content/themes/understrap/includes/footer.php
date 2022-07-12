4806aa9fd91fe2e4378b01762b493d3fd65ecb10
King Wai <kwlai121@gmail.com>
2022-07-11 22:05:40 -0600

    wp blog added

---
+++ blog/wp-content/themes/understrap/includes/footer.php
@@ -0,0 +1,40 @@
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row py-5">    
+        <div class="col-12 col-md-5 py-4"> 
+          <img src="img/logoFooter.png" alt="">
+        </div>
+       
+        <div class="col-12 col-md-3 pt-sm-5 pt-xs-4 d-flex justify-content-lg-end"> 
+          <p><i class="fa-brands fa-whatsapp"></i> +506 7111-5516</p>
+        </div>
+        <div class="col-12 col-md-4 pt-sm-5 pt-xs-4 d-flex justify-content-lg-end"> 
+          
+          <p><i class="fa-solid fa-envelope"></i> info@cxtotal.com</p>
+        </div> 
+      </div>
+
+      <hr class="my-3 mx-2 bg-light">
+
+      <div class="row">
+
+        <div class="col-12 col-sm-7 d-flex">
+          <p>Derechos Reservados. CX Total S.A. 2022</p>
+        </div>
+        <div class="col-12 col-sm-5 d-flex justify-content-sm-end">
+          <a href="#">Política de privacidad</a>
+        </div>
+
+      </div>
+
+    </div> <!-- End container -->
+  </footer>   
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js" integrity="sha256-8WqyJLuWKRBVhxXIL1jBDD7SDxU936oZkCnxQbWwJVw=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script><!--<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>-->
+
+</html>
