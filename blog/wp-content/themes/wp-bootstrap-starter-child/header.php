86563cb0d9a0fdf218c9a0643a48e09a7e0a77d8
King Wai <kwlai121@gmail.com>
2022-07-12 23:26:15 -0600

    blocg header and footer fixed

--- blog/wp-content/themes/wp-bootstrap-starter-child/header.php
+++ blog/wp-content/themes/wp-bootstrap-starter-child/header.php
@@ -9,7 +9,54 @@
  * @package WP_Bootstrap_Starter
  */
 
-?><!DOCTYPE html>
+?>
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'CX Total - Formulario de contáctenos'; 
+		$to = 'kwlai121@gmail.com'; 
+		$subject = 'Mensaje de Contact CXtotal';
+		
+		$body ="From: $name\n Correo Electrónico: $email\n Mensaje:\n $message";
+
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Por favor, escriba su nombre';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Por favor, introduce una dirección de correo electrónico válida';
+		}
+		// Check if number has been entered and is valid
+		if (!$_POST['number']) {
+			$errNumber = 'Por favor, introduce su numero de telefono';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Por favor ingrese su mensaje';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">¡Gracias! estaré en contacto</div>';
+	} else {
+		$result='<div class="alert alert-danger">Lo siento, hubo un error al enviar tu mensaje. Por favor, inténtelo de nuevo más tarde.</div>';
+	}
+}
+	}
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -54,6 +101,76 @@
 						<i class="fas fa-bars"></i>
 					</button>
 				</div>
+				    <!--MODAL CONTACTENOS-->
+    <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Contáctenos</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+          <form class="form-horizontal" role="form" method="post" action="index.php">
+					<div class="form-group">
+						<label for="name" class="col-sm-12 control-label">Nombre completo</label>
+						<div class="col-sm-12">
+							<input type="text" class="form-control" id="name" name="name" placeholder="Digite su nombre completo" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+							<?php echo "<p class='text-danger'>$errName</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email" class="col-sm-12 control-label">Correo electrónico</label>
+						<div class="col-sm-12">
+							<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+							<?php echo "<p class='text-danger'>$errEmail</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="number" class="col-sm-12 control-label">Teléfono</label>
+						<div class="col-sm-12">
+							<input type="number" class="form-control" id="number" name="number" placeholder="+000 0000 0000" value="<?php echo htmlspecialchars($_POST['number']); ?>">
+							<?php echo "<p class='text-danger'>$errNumber</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message" class="col-sm-12 control-label">Mensaje</label>
+						<div class="col-sm-12">
+							<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+							<?php echo "<p class='text-danger'>$errMessage</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message" class="col-sm-12 control-label">País</label>
+						<div class="col-sm-12">
+							<select name="" id="people">
+
+              </select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="human" class="col-sm-12 control-label">2 + 3 = ?</label>
+						<div class="col-sm-12">
+							<input type="text" class="form-control" id="human" name="human" placeholder="Tu respuesta">
+							<?php echo "<p class='text-danger'>$errHuman</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-12">
+							<input id="submit" name="submit" type="submit" value="Enviar" class="btn btn-primary">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-12 col-sm-offset-2">
+							<?php echo $result; ?>	
+						</div>
+					</div>
+				</form> 
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!--END MODAL CONTACTENOS-->
                 <?php
                 wp_nav_menu(array(
                 'theme_location'    => 'primary',
